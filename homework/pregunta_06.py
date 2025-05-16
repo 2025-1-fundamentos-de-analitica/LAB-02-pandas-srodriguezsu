577b83d2981f0da44df10881fde71364da913280
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-05-16 13:15:52 -0500

    Intento 1

--- homework/pregunta_06.py
+++ homework/pregunta_06.py
@@ -4,14 +4,17 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
+import pandas as pd
 
 
 def pregunta_06():
     """
     Retorne una lista con los valores unicos de la columna `c4` del archivo
-    `tbl1.csv` en mayusculas y ordenados alfabéticamente.
+    `tbl1.tsv` en mayusculas y ordenados alfabéticamente.
 
     Rta/
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
+    df = pd.read_csv("files/input/tbl1.tsv", sep="\t")
+    return list(sorted(df["c4"].str.upper().unique()))
